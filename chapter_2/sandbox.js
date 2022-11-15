d67b065a1079b8e8629895196716680a01a04523
soran <soran.taak@gmail.com>
2022-11-15 19:58:26 +0330

    boolean and comparison

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,6 +1,27 @@
-// Null & Undefined
-let age;
-console.log(age, age + 3, `the age is ${age}`);
+// Boolean and Comparison
+console.log(true, false, "true", "false");
 
-let fName = null;
-console.log(fName, fName + 3, `the name is ${fName}`);
+// methods can return booleans
+let email = "soran.taak@gmail.com";
+let names = ["soran", "sahar", "mahsa"];
+let result = email.includes("@");
+console.log(result);
+let result2 = names.includes("mahsaa");
+console.log(result2);
+// compariso operators
+let age = 25;
+console.log(age == "25");
+console.log(age == 25);
+console.log(age === "25");
+console.log(age !== 30);
+console.log(age > 30);
+console.log(age >= 30);
+console.log(age <= 30);
+
+let fName = "soran";
+console.log(fName == "soran");
+console.log(fName == "Soran");
+console.log(fName > "taak");
+console.log(fName < "taak");
+console.log(fName < "Soran");
+console.log(fName > "Soran");
