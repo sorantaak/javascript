6271f97ecff50d043afa5461fc9e0a0ecc01a5d8
soran <soran.taak@gmail.com>
2022-11-15 21:34:17 +0330

    if statement

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,12 +1,17 @@
-//do while loops
-const fNames = ["soran", "sahar", "sirwan", "mahsa"];
-let i = 0;
-do {
-	console.log("value fo i is: ", i);
-	i++;
-} while (i < 5);
-// let i = 0;
-// while (i < fNames.length) {
-// 	console.log(i, fNames[i]);
-// 	i++;
+// if statements
+// const age = 25;
+
+// if (age > 20) {
+// 	console.log("you are ower 20 years old");
 // }
+
+// const fighters = ["soran", "sahar", "sirwan", "mahsa"];
+
+// if (fighters.length > 4) {
+// 	console.log("that's a lot of fighters");
+// }
+const password = "password";
+
+if (password.length >= 8) {
+	console.log("that password is log enough!");
+}
