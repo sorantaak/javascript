36a36efb42a6024378c0262ef6151ec48811eb5e
soran <soran.taak@gmail.com>
2022-11-15 21:26:26 +0330

    do while loop

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,12 +1,12 @@
-// while loops
+//do while loops
 const fNames = ["soran", "sahar", "sirwan", "mahsa"];
+let i = 0;
+do {
+	console.log("value fo i is: ", i);
+	i++;
+} while (i < 5);
 // let i = 0;
-// while (i < 5) {
-// 	console.log("in loop: ", i);
+// while (i < fNames.length) {
+// 	console.log(i, fNames[i]);
 // 	i++;
 // }
-let i = 0;
-while (i < fNames.length) {
-	console.log(i, fNames[i]);
-	i++;
-}
