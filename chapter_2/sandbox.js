aad5a85beb768d722419a388b533cc9417141620
soran <soran.taak@gmail.com>
2022-11-15 20:08:48 +0330

    loos vs strict comparison

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,27 +1,13 @@
-// Boolean and Comparison
-console.log(true, false, "true", "false");
-
-// methods can return booleans
-let email = "soran.taak@gmail.com";
-let names = ["soran", "sahar", "mahsa"];
-let result = email.includes("@");
-console.log(result);
-let result2 = names.includes("mahsaa");
-console.log(result2);
-// compariso operators
 let age = 25;
-console.log(age == "25");
-console.log(age == 25);
-console.log(age === "25");
-console.log(age !== 30);
-console.log(age > 30);
-console.log(age >= 30);
-console.log(age <= 30);
 
-let fName = "soran";
-console.log(fName == "soran");
-console.log(fName == "Soran");
-console.log(fName > "taak");
-console.log(fName < "taak");
-console.log(fName < "Soran");
-console.log(fName > "Soran");
+// loos comparison (diffrent types can still be equal)
+// console.log(age == 25);
+// console.log(age == "25");
+// console.log(age != "25");
+// console.log(age != 25);
+
+// strict comparison (diffrent types cannot be equal)
+console.log(age === 25);
+console.log(age === '25');
+console.log(age !== '25');
+console.log(age !==  25);
