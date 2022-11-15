c90a5e372c504835f3bae14768f3253230d52f8e
soran <soran.taak@gmail.com>
2022-11-15 09:39:22 +0330

    Variables, Constants & Comments

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,2 +1,18 @@
-console.log(1);
-console.log(2);
+let age = 25;
+let year = 2019;
+
+//log things to console
+console.log(age, year);
+
+/* multi line 
+
+comments */
+age = 30;
+
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+var score = 75;
+console.log(score);
