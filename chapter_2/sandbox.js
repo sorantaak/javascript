ae029d0191598242d715891788171196bfda8227
soran <soran.taak@gmail.com>
2022-11-15 20:20:37 +0330

    Type conversion

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,13 +1,14 @@
-let age = 25;
+// type conversion
+let score = "100";
 
-// loos comparison (diffrent types can still be equal)
-// console.log(age == 25);
-// console.log(age == "25");
-// console.log(age != "25");
-// console.log(age != 25);
+// score = Number(score);
+// console.log(score + 1);
+// console.log(typeof score);
 
-// strict comparison (diffrent types cannot be equal)
-console.log(age === 25);
-console.log(age === '25');
-console.log(age !== '25');
-console.log(age !==  25);
+// let result = Number('hello');
+// let result = String(50);
+// let result = Boolean(100);
+// let result = Boolean(0);
+let result = Boolean('aa')
+
+console.log(result, typeof result); 
