65dd7a1797922750db4972b853b5841a4360c482
soran <soran.taak@gmail.com>
2022-11-16 14:42:13 +0330

    return values from functions

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,9 +1,14 @@
-// arguments & parametrs
+// returning values
 
-const speak = function (name = "soran", time = "night") {
-	console.log(`good ${time} ${name}`);
+// const speak = function (name = "soran", time = "night") {
+// 	console.log(`good ${time} ${name}`);
+// };
+const calcArea = function (radius) {
+	// let area
+	return 3.14 * radius ** 2;
+	// return area;
 };
 
-speak();
-speak("sahar", "morning");
-speak("mahsa");
+const area = calcArea(5);
+console.log(area);
+ 
