e58ee4813b483e4a489ec58217242d9891d0f22e
soran <soran.taak@gmail.com>
2022-11-16 12:05:47 +0330

    breack and continue

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,8 +1,15 @@
-// Logic Not (!)
-let user = false;
-if (!user) {
-	console.log("you must be logged in to continue");
-}
+// break and continue
 
-console.log(!true);
-console.log(!false);
+const score = [50, 25, 0, 30, 100, 20, 10];
+
+for (let i = 0; i < score.length; i++) {
+	if(score[i]===0){
+		console.log('your score is zero');
+		continue;
+	}
+	console.log("your score: ", score[i]);
+	if (score[i] === 100) {
+		console.log("congrats, tou got the top score!");
+		break;
+	}
+}
