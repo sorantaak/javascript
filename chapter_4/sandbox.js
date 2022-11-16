5d0f564685becf02516242327f4de491712adc83
soran <soran.taak@gmail.com>
2022-11-16 18:39:58 +0330

    Functions vs methods

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,27 +1,10 @@
-// practice arrow functions function
-// const greet = function(){
-//     return 'hello, world';
-// }
+const fname = "soran";
 
-const greet = () => "hello, wrold";
-const result = greet();
-console.log(result);
+// functions
+const greet = () => "hello";
+let resultOne = greet();
+console.log(resultOne);
 
-const bill = function (products, tax) {
-	let total = 0;
-	for (let i = 0; i < products.length; i++) {
-		total += products[i] + products[i] * tax;
-	}
-	return total;
-};
-// bill and bill2 same function
-const bill2 = (products, tax) => {
-	let total = 0;
-	for (let i = 0; i < products.length; i++) {
-		total += products[i] + products[i] * tax;
-	}
-	return total;
-};
-
-console.log(bill([10, 15, 30], 0.2));
-console.log(bill2([10, 15, 30], 0.2));
+// methods
+let resultTwo = fname.toUpperCase();
+console.log(resultTwo);
