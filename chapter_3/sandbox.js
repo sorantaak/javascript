8fff5d935e64e0abeaeb72d7007f0c5c63cccc25
soran <soran.taak@gmail.com>
2022-11-16 11:57:52 +0330

    Logic not operators

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,12 +1,8 @@
-// Logic operators - OR || and nd &&
-const password = "p@ss12";
-if (password.length >= 12 && password.includes("@")) {
-	console.log("that password is mighty strong");
-} else if (
-	password.length >= 8 ||
-	(password.includes("@") && password.length > 5)
-) {
-	console.log("that password is strong enough!");
-} else {
-	console.log("password is not strong enogh!");
+// Logic Not (!)
+let user = false;
+if (!user) {
+	console.log("you must be logged in to continue");
 }
+
+console.log(!true);
+console.log(!false);
