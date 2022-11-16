080bf7721240d0f122daaa296b3bbe036d586bb1
soran <soran.taak@gmail.com>
2022-11-16 14:05:47 +0330

    function declaration and expression

---
+++ chapter_4/sandbox.js
@@ -0,0 +1,18 @@
+// function declaration
+
+// function expression
+const speak = function () {
+	console.log("good day");
+};
+
+// greet();
+// greet();
+
+speak();
+speak();
+speak();
+
+function greet() {
+	console.log("hello there");
+}
+// notice: function expression should be define befor voked it but function decleration do not have this rule.
