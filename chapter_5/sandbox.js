263774404a17a72827bcdd48730dfddc673a9170
soran <soran.taak@gmail.com>
2022-11-17 18:48:38 +0330

    Math Object

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,33 +1,24 @@
-// Object literals
-// const blogs = [
-// 	{ title: "why mac & chees rules", likes: 30 },
-// 	{ title: "10 things make with marmite", likes: 50 },
-// ];
-// console.log(blogs);
-let user = {
-	name: "soran",
-	age: 41,
-	email: "soran.taak@gmail.com",
-	location: "Mahabad",
-	blogs: [
-		{ title: "why mac & chees rules", likes: 30 },
-		{ title: "10 things make with marmite", likes: 50 },
-	],
-	login() {
-		console.log("the user loged in!");
-	},
-	logout() {
-		console.log("the user loged out");
-	},
-	logBlogs: function () {
-		console.log("this user has written the following blogs:");
-		this.blogs.forEach((blog) => {
-			console.log(blog.title , blog.likes);
-		});
-	},
-	logBlogs2: () => {
-		console.log(this); // this key word in arrow functin that related to object refers the window object
-	},
-};
+// Math Object
 
-user.logBlogs();
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.7;
+const area2 = 7.2;
+console.log(Math.round(area));
+console.log(Math.round(area2));
+
+console.log(Math.floor(area));
+console.log(Math.floor(area2));
+
+console.log(Math.ceil(area));
+console.log(Math.ceil(area2));
+
+console.log(Math.trunc(area));
+console.log(Math.trunc(area2));
+
+// random numbers
+const random = Math.random(); // random number is between 0 to  1
+console.log(random);
+console.log(Math.round(random * 100)); // retrun random number between 0 to 100
