6c19215b198706aef79eb4ab547962bd809d5528
soran <soran.taak@gmail.com>
2022-11-17 19:15:53 +0330

    Premitive vs Refrecnce Types

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,24 +1,17 @@
-// Math Object
+// primitive values
+// let scoreOne = 50;
+// let scoreTwo = scoreOne;
 
-console.log(Math);
-console.log(Math.PI);
-console.log(Math.E);
+// console.log(`scoreObe : ${scoreOne}`, `scorreTwo:${scoreTwo}`);
 
-const area = 7.7;
-const area2 = 7.2;
-console.log(Math.round(area));
-console.log(Math.round(area2));
+// scoreOne = 100;
+// console.log(`scoreObe : ${scoreOne}`, `scorreTwo:${scoreTwo}`);
 
-console.log(Math.floor(area));
-console.log(Math.floor(area2));
+// refrence values
 
-console.log(Math.ceil(area));
-console.log(Math.ceil(area2));
+const userOne = { name: "soran", age: 30 };
+const userTwo = userOne;
 
-console.log(Math.trunc(area));
-console.log(Math.trunc(area2));
-
-// random numbers
-const random = Math.random(); // random number is between 0 to  1
-console.log(random);
-console.log(Math.round(random * 100)); // retrun random number between 0 to 100
+console.log(userOne, userTwo);
+userTwo.age = 40;
+console.log(userOne, userTwo);
