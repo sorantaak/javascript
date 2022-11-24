1c3ac26d82ed4fbf301ae9fdc250743d1bd18eb5
soran <soran.taak@gmail.com>
2022-11-24 20:17:16 +0330

    More Events

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,32 +1,14 @@
-// const button = document.querySelector("button");
-// button.addEventListener("click", () => {
-// 	console.log("you clicked me");
-// });
-const ul = document.querySelector("ul");
-const button = document.querySelector("button");
-
-button.addEventListener("click", () => {
-	// ul.innerHTML += "<li>something new</li>";
-	const li = document.createElement("li");
-	li.textContent = "something new to do";
-	// ul.append(li);
-	ul.prepend(li);
+const copy = document.querySelector(".copy-me");
+const box = document.querySelector(".box");
+copy.addEventListener("copy", () => {
+	console.log("Oi , my content is copyright!!");
 });
-// const lists = document.querySelectorAll("li");
 
-// lists.forEach((item) => {
-// 	item.addEventListener("click", (e) => {
-// 		console.log("event in li");
-// 		e.stopPropagation();
-// 		e.target.remove();
-// 	});
-// });
+box.addEventListener("mousemove", (e) => {
+	// console.log(e.offsetX, e.offsetY);
+	box.textContent = `x pos = ${e.offsetX} y pos = ${e.offsetY}`;
+});
 
-ul.addEventListener("click", (e) => {
-	// console.log("event in Ul");
-	// console.log(e.target);
-	console.log(e.target.tagName);
-	if (e.target.tagName === "LI") {
-		e.target.remove();
-	}
+document.addEventListener("wheel", (e) => {
+	console.log(e.pageX, e.pageY);
 });
