ca3675675fc5e6d1329f9ea376f914a56b9a8de4
soran <soran.taak@gmail.com>
2022-11-24 17:38:16 +0330

    Creating and Removing Elements

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -2,7 +2,16 @@
 // button.addEventListener("click", () => {
 // 	console.log("you clicked me");
 // });
+const ul = document.querySelector("ul");
+const button = document.querySelector("button");
 
+button.addEventListener("click", () => {
+	// ul.innerHTML += "<li>something new</li>";
+	const li = document.createElement("li");
+	li.textContent = "something new to do";
+	// ul.append(li);
+	ul.prepend(li);
+});
 const lists = document.querySelectorAll("li");
 
 lists.forEach((item) => {
@@ -10,7 +19,8 @@ lists.forEach((item) => {
 		// console.log(e);
 		// console.log(e.target);
 		// console.log(item);
-		e.target.style.textDecoration = "line-through";
+		// e.target.style.textDecoration = "line-through";
+		e.target.remove();
 		// item.remove();
 	});
 });
