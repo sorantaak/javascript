603b92e72bec8ede69eebfffbb1c12bb18670817
soran <soran.taak@gmail.com>
2022-11-25 22:23:36 +0330

    Testing RegEx Pattern

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -6,3 +6,16 @@ form.addEventListener("submit", (e) => {
 	// console.log(username.value);
 	console.log(form.username.value);
 });
+
+const username = "343434soradf";
+const pattern = /^[a-z]{6,}$/;
+
+// let result = pattern.test(username)
+// if(result){
+//     console.log('regex test passed :)');
+// }else{
+//       console.log("regex test faild :( ");
+// }
+
+let result = username.search(pattern); //return integer if pattern is not match, search function return -1 else return positon letter
+console.log(result);
