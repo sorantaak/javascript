897d378704df97147375273513c95bb8297de94d
sorantaak <soran.taak@gmail.com>
2022-11-26 01:25:34 +0330

    Keyboard events

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -1,12 +1,12 @@
 const form = document.querySelector(".signup-form");
 const feedback = document.querySelector(".feedback");
+const usernamePattern = /^[a-zA-z]{6,12}$/;
+
+
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-
 	//validation
 	const username = form.username.value;
-	const usernamePattern = /^[a-zA-z]{6,12}$/;
-
 	if (usernamePattern.test(username)) {
 		// feedback good info
 		feedback.textContent = "that username is valid!";
@@ -14,5 +14,17 @@ form.addEventListener("submit", (e) => {
 		// feedback help info
 		feedback.textContent =
 			"username must contain letters only & between 6 & 12 characters long";
+	}
+});
+
+// live feedback
+form.username.addEventListener("keyup", (e) => {
+	// console.log(e.target.value , form.username.value);
+	console.log(e);
+	if(usernamePattern.test(e.target.value)){
+		console.log('passed');
+		form.username.setAttribute('class','success')
+	}else{
+		form.username.setAttribute('class','error')
 	}
 });
