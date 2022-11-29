b2e7c3beafa61079169dce013682eccacf941bb2
soran <soran.taak@gmail.com>
2022-11-29 17:46:53 +0330

    The window Object

--- ninja_quiz/app.js
+++ ninja_quiz/app.js
@@ -18,6 +18,7 @@ form.addEventListener("submit", (e) => {
 		}
 
 		//show result on page
+		scrollTo(0, 0);
 		result.querySelector("span").textContent = `${score}%`;
 		result.classList.remove("d-none");
 		console.log(score);
