b64ad0563507684e0a10bc1c0421aa90365011f4
soran <soran.taak@gmail.com>
2022-11-30 21:05:57 +0330

    sort array method

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,8 +1,38 @@
-// find mehtod
-const scores = [10, 5, 0, 40, 60, 10, 20, 70];
+// example 1 - sorting strings
+const names = ["mario", "shaun", "chun-li", "luigi"];
+names.sort();
+console.log(names); // sort method unlike others array mehtods , alter the original array
 
-const scoreHighValue = scores.find((score) => {
-	return score > 50;
-}); // return first value if condition is true otherwise return undifined
+// example 2 = sorting numbers
+const scores = [10, 50, 20, 5, 35, 70, 45];
 
-console.log(scoreHighValue);
+// scores.sort((a, b) => {
+// 	if (a > b) {
+// 		return 1;
+// 	} else if (a < b) {
+// 		return -1;
+// 	} else {
+// 		return 0;
+// 	}
+// });
+
+scores.sort((a, b) => {
+	return a - b;
+});
+
+console.log(scores);
+
+// exalple3 - sorting objects
+const players = [
+	{ name: "mario", score: 20 },
+	{ name: "luigi", score: 10 },
+	{ name: "chunli", score: 50 },
+	{ name: "yoshi", score: 30 },
+	{ name: "shaun", score: 70 },
+];
+
+players.sort((a, b) => {
+	return a.score - b.score;
+});
+
+console.log(players);
