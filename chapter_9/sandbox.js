95462696a75a22d73231dc80560b1c058e86b8a5
soran <soran.taak@gmail.com>
2022-11-30 19:57:21 +0330

    reduce method

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,30 +1,38 @@
-// map method
-const prices = [20, 10, 30, 25, 15, 40, 80, 5];
+// reduce method
+const scores = [10, 20, 60, 40, 70, 90, 30];
 
-// const salePrices = prices.map((price) => {
-// 	return price / 2;
-// });
-const salePrices = prices.map((price) => price / 2);
-
-console.log("original prices: ", prices);
-console.log("salePrices", salePrices);
-
-const products = [
-	{ name: "gold star", prices: 20 },
-	{ name: "mushroom", prices: 40 },
-	{ name: "green shells", prices: 30 },
-	{ name: "banan skin", prices: 10 },
-	{ name: "red shells", prices: 50 },
+const result = scores.reduce((acc, curr) => {
+	if (curr > 50) {
+		acc++;
+	}
+	return acc;
+}, 0);
+console.log(result);
+// in above example if do not set second argument in reduce method then acc set first element valuted by score arra y acc=10
+const scores2 = [
+	{ player: "mario", score: 50 },
+	{ player: "yoshi", score: 30 },
+	{ player: "mario", score: 70 },
+	{ player: "crystal", score: 60 },
+	{ player: "mario", score: 50 },
+	{ player: "yoshi", score: 30 },
+	{ player: "mario", score: 70 },
+	{ player: "crystal", score: 60 },
+	{ player: "mario", score: 50 },
+	{ player: "yoshi", score: 30 },
+	{ player: "mario", score: 70 },
+	{ player: "crystal", score: 60 },
+	{ player: "mario", score: 50 },
+	{ player: "yoshi", score: 30 },
+	{ player: "mario", score: 70 },
+	{ player: "crystal", score: 60 },
 ];
 
-const saleProduct = products.map((product) => {
-	if (product.prices > 30) {
-		// product.prices = product.prices / 2; // this statemnt cuse alter original porduct object beacuse each items in producs array are object
-		// return product;
-		return { name: product.name, price: product.prices / 2 };
-	} else {
-		return product;
+const res = scores2.reduce((acc, curr) => {
+	if (curr.player === "mario") {
+		acc += curr.score;
 	}
-});
+	return acc;
+}, 0);
 
-console.log(saleProduct);
+console.log("mario totla scores is :", res);
