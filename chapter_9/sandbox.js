2900bc47dff0f818da552b36a65330ebf82f5e00
soran <soran.taak@gmail.com>
2022-11-30 20:06:38 +0330

    find method

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,38 +1,8 @@
-// reduce method
-const scores = [10, 20, 60, 40, 70, 90, 30];
+// find mehtod
+const scores = [10, 5, 0, 40, 60, 10, 20, 70];
 
-const result = scores.reduce((acc, curr) => {
-	if (curr > 50) {
-		acc++;
-	}
-	return acc;
-}, 0);
-console.log(result);
-// in above example if do not set second argument in reduce method then acc set first element valuted by score arra y acc=10
-const scores2 = [
-	{ player: "mario", score: 50 },
-	{ player: "yoshi", score: 30 },
-	{ player: "mario", score: 70 },
-	{ player: "crystal", score: 60 },
-	{ player: "mario", score: 50 },
-	{ player: "yoshi", score: 30 },
-	{ player: "mario", score: 70 },
-	{ player: "crystal", score: 60 },
-	{ player: "mario", score: 50 },
-	{ player: "yoshi", score: 30 },
-	{ player: "mario", score: 70 },
-	{ player: "crystal", score: 60 },
-	{ player: "mario", score: 50 },
-	{ player: "yoshi", score: 30 },
-	{ player: "mario", score: 70 },
-	{ player: "crystal", score: 60 },
-];
+const scoreHighValue = scores.find((score) => {
+	return score > 50;
+}); // return first value if condition is true otherwise return undifined
 
-const res = scores2.reduce((acc, curr) => {
-	if (curr.player === "mario") {
-		acc += curr.score;
-	}
-	return acc;
-}, 0);
-
-console.log("mario totla scores is :", res);
+console.log(scoreHighValue);
