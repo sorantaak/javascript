84d0a8eee869906f3da93a035fb7b8f7534c961c
soran <soran.taak@gmail.com>
2022-12-06 19:31:18 +0330

    Response and Status

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -2,9 +2,11 @@ const requset = new XMLHttpRequest();
 
 requset.addEventListener("readystatechange", () => {
 	// console.log(requset, requset.readyState, requset.status);
-	if (requset.readyState === 4) {
-		console.log(requset.responseText);
+	if (requset.readyState === 4 && requset.status === 200) {
+		console.log(requset, requset.responseText);
+	} else if (requset.readyState === 4) {
+		console.log("could not fetch the data");
 	}
 });
-requset.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+requset.open("GET", "https://jsonplaceholder.typicode.com/todoss/");
 requset.send();
