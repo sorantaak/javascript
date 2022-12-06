eb3458475135967e40abf15f454b61186d8b920f
soran <soran.taak@gmail.com>
2022-12-06 19:48:45 +0330

    Callback Functions

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,12 +1,31 @@
-const requset = new XMLHttpRequest();
+const getTodos = (callback) => {
+	const requset = new XMLHttpRequest();
 
-requset.addEventListener("readystatechange", () => {
-	// console.log(requset, requset.readyState, requset.status);
-	if (requset.readyState === 4 && requset.status === 200) {
-		console.log(requset, requset.responseText);
-	} else if (requset.readyState === 4) {
-		console.log("could not fetch the data");
+	requset.addEventListener("readystatechange", () => {
+		// console.log(requset, requset.readyState, requset.status);
+		if (requset.readyState === 4 && requset.status === 200) {
+			callback(undefined, requset.responseText);
+		} else if (requset.readyState === 4) {
+			callback("could not fetch the data", undefined);
+		}
+	});
+	requset.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+	requset.send();
+};
+console.log(1);
+console.log(2);
+getTodos((err, data) => {
+	console.log("callback fired");
+	// console.log(err, data);
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(data);
 	}
 });
-requset.open("GET", "https://jsonplaceholder.typicode.com/todoss/");
-requset.send();
+console.log(3);
+console.log(4);
+
+// getTodos(()=>{
+//     console.log('another callback');
+// });
