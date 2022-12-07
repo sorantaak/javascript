041bae3fe8dd360d21e40e2dfce07e8643cd6795
soran <soran.taak@gmail.com>
2022-12-07 11:36:03 +0330

    Cahining Promise

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -15,9 +15,17 @@ const getTodos = (resource) => {
 	});
 };
 
-getTodos("todos/luigid.json")
+getTodos("todos/luigi.json")
 	.then((data) => {
-		console.log("Promise resolved: ", data);
+		console.log("Promise 1 resolved: ", data);
+		return getTodos("todos/mario.json");
+	})
+	.then((data) => {
+		console.log("Promise 2 resolved: ", data);
+		return getTodos("todos/shaun.json");
+	})
+	.then((data) => {
+		console.log("Promise 3 resolved: ", data);
 	})
 	.catch((err) => {
 		console.log("Promise rejected: ", err);
