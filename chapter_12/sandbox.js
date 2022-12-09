cddb3e6973516333f8a8e8e94f6b5d1980767c1f
soran <soran.taak@gmail.com>
2022-12-09 12:41:02 +0330

    Throwing & Cathcing Error

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,14 +1,22 @@
 // async & await
 const getTodos = async () => {
 	const response = await fetch("todos/luigi.json");
+	if (response.status !== 200) {
+		throw new Error("can not access to url");
+	}
+	console.log("test");
 	const data = await response.json();
 	return data;
 };
 console.log(1);
 console.log(2);
-getTodos().then((data) => {
-	console.log(data);
-});
+getTodos()
+	.then((data) => {
+		console.log(data);
+	})
+	.catch((err) => {
+		console.log("rejected: ", err.message);
+	});
 console.log(3);
 console.log(4);
 
