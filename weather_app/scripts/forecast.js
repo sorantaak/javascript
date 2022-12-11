da782615fb5fd998a2c2b776fda2a419a45c5a8e
soran <soran.taak@gmail.com>
2022-12-11 12:05:56 +0330

    Get City Api Call

--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -1,1 +1,18 @@
-const key = "JKVqUREuMzONa69dRIpdLu7XGlUUgRwh";
+const key = "PsuiIsujnuGAzgmstRztGTZUyCQytTa4";
+
+const getCity = async (city) => {
+	const baseUrl =
+		"http://dataservice.accuweather.com/locations/v1/cities/search";
+	const query = `?apikey=${key}&q=${city}`;
+	const response = await fetch(baseUrl + query);
+	const data = await response.json();
+	return data[0];
+};
+
+getCity("mahabad")
+	.then((data) => {
+		console.log(data);
+	})
+	.catch((err) => {
+		console.log(err.message);
+	});
