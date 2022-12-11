070c6b40a80f5afc95e831c279a79f8fceb2ada8
soran <soran.taak@gmail.com>
2022-12-11 20:11:38 +0330

    Updating the Locatin

--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -19,15 +19,4 @@ const getCity = async (city) => {
 	return data[0];
 };
 
-getCity("london")
-	.then((data) => {
-		return getWeather(data.Key);
-	})
-	.then((data) => {
-		console.log(data);
-	})
-	.catch((err) => {
-		console.log(err.message);
-	});
-
-getWeather("329260");
+//   
