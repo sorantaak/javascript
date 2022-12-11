16bdc272756fd0b9c0b2bb8e4ac9bc6e78ca2adc
soran <soran.taak@gmail.com>
2022-12-12 01:12:37 +0330

    Object Shorthand Notation

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -3,10 +3,7 @@ const cityForm = document.querySelector("form");
 const updateCity = async (city) => {
 	const cityDets = await getCity(city);
 	const weather = await getWeather(cityDets.Key);
-	return {
-		cityDets: cityDets,
-		weather: weather,
-	};
+	return { cityDets, weather };
 };
 
 cityForm.addEventListener("submit", (e) => {
