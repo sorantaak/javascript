2b9b5bc0ba13164698348e5359d54ca3525c3673
soran <soran.taak@gmail.com>
2022-12-12 16:59:05 +0330

    Weather Icons & images

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -1,9 +1,12 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 // update UI
 const updateUi = (data) => {
+	console.log(data);
 	// destructure propertise
 	const { cityDets, weather } = data;
 
@@ -16,6 +19,18 @@ const updateUi = (data) => {
 			<span>&deg;C</span>
 		</div>
 	`;
+
+	// update the night/day & icon images
+	const iconSrc = `icons/${weather.WeatherIcon}.svg`;
+	icon.setAttribute("src", iconSrc);
+
+	let timeSrc = null;
+	if (weather.IsDayTime) {
+		timeSrc = "img/day.svg";
+	} else {
+		timeSrc = "img/night.svg";
+	}
+	time.setAttribute("src", timeSrc);
 
 	// remove d-none class if present
 	if (card.classList.contains("d-none")) {
