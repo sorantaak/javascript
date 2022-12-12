27ef7ecfbdbf11a8996e5bb058a375f6112aa1d4
soran <soran.taak@gmail.com>
2022-12-12 16:09:55 +0330

    Updating the Ui

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -1,4 +1,26 @@
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+
+const updateUi = (data) => {
+	const cityDets = data.cityDets;
+	const weather = data.weather;
+
+	//update detail template
+	details.innerHTML = `
+		<h5 class="my-3">${cityDets.EnglishName}</h5>
+		<div class="my-3">${weather.WeatherText}</div>
+		<div class="display-4 my-4">
+			<span>${weather.Temperature.Metric.Value}</span>
+			<span>&deg;C</span>
+		</div>
+	`;
+
+	// remove d-none class if present
+	if (card.classList.contains("d-none")) {
+		card.classList.remove("d-none");
+	}
+};
 
 const updateCity = async (city) => {
 	const cityDets = await getCity(city);
@@ -16,6 +38,6 @@ cityForm.addEventListener("submit", (e) => {
 
 	//update the ui with new city
 	updateCity(city)
-		.then((data) => console.log(data))
+		.then((data) => updateUi(data))
 		.catch((err) => console.log(err));
 });
