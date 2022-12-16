553f44662002ddc6af842c9232300dc08fa26690
soran <soran.taak@gmail.com>
2022-12-16 18:17:43 +0330

    class methods & method chaining

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -5,12 +5,29 @@ class User {
 		// set up properties
 		this.username = username;
 		this.email = email;
+		this.score = 0;
+	}
+
+	login() {
+		console.log(`${this.username} just logged in`);
+		return this;
+	}
+	logout() {
+		console.log(`${this.username} just logged out`);
+		return this;
+	}
+	incScore() {
+		this.score++;
+		console.log(`${this.username} has a score of ${this.score}`);
+		return this;
 	}
 }
 
 const userOne = new User("soran", "soran@gmailcom");
 const userTwo = new User("luigi", "luigi@gmail.com");
+
 console.log(userOne, userTwo);
+userOne.login().incScore().incScore().logout();
 
 // the 'new' keyword
 // 1 - it creates a new empty object {}
