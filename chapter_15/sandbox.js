39fa6be084c1b98b843085b31704e812cca1da79
soran <soran.taak@gmail.com>
2022-12-16 21:40:42 +0330

    Constuctors (under the hood)

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -1,41 +1,26 @@
-// class
-
-class User {
-	constructor(username, email) {
-		// set up properties
-		this.username = username;
-		this.email = email;
-		this.score = 0;
-	}
-
-	login() {
-		console.log(`${this.username} just logged in`);
-		return this;
-	}
-	logout() {
-		console.log(`${this.username} just logged out`);
-		return this;
-	}
-	incScore() {
-		this.score++;
-		console.log(`${this.username} has a score of ${this.score}`);
-		return this;
-	}
+// constractor function
+function User(username, email) {
+	this.username = username;
+	this.email = email;
+	this.login = function () {
+		console.log(`${this.username} logged in`);
+	};
 }
-
-class Admin extends User {
-	constructor(username, email, title) {
-		super(username, email);
-		this.title = title;
-	}
-	deleteUser(user) {
-		users = users.filter((u) => u.username !== user.usernam);
-	}
-}
+// class User {
+// 	constructor(username, email) {
+// 		// set up properties
+// 		this.username = username;
+// 		this.email = email;
+// 		this.score = 0;
+// 	}
+// }
 
 const userOne = new User("soran", "soran@gmailcom");
 const userTwo = new User("luigi", "luigi@gmail.com");
-const userThree = new Admin("sirwan", "sirwan@gmail.com", "black-belt-ninja");
+console.log(userOne, userTwo);
+userOne.login();
 
-console.log(userThree);
-let users = [userOne, userTwo, userThree];
+// the 'new' keyword
+// 1 - it creates a new empty object {}
+// 2 - it binds the value of 'this' to the new empty object
+// 3 - it calls the constructor function to 'build' the object
