77b67da7561ffe83bfa782e6e249c363741c80a0
soran <soran.taak@gmail.com>
2023-01-01 19:52:44 +0330

    another way to use call - apply and bind method

--- call-apply-bind/app.js
+++ call-apply-bind/app.js
@@ -1,6 +1,10 @@
 const car = {
 	name: "saipa",
 	model: "111",
+	showCar: function (year, month) {
+		console.log(year, month);
+		console.log(`show car by name ${this.name} and ${this.model}`);
+	},
 };
 
 const car2 = {
@@ -15,11 +19,15 @@ function displayDeatails(year, month) {
 
 // apply
 displayDeatails.apply(car, [2022, 12]);
+car.showCar.apply(car2, [1965, 40]); // another way to use aplly
 
 //call
 displayDeatails.call(car2, 2021, 8);
+car.showCar.call(car2, 2024, 1945);
 
 //bind
 displayDeatails.bind({ name: "peykan", model: "147" }, 1981, 4)();
 const a = displayDeatails.bind({ name: "renualt", model: "2000" }, 2000, 4);
 a();
+
+car.showCar.bind({ name: "toyota", model: "500" }, 1000, 5)();
