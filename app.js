e7134be9ee4e670915c1fefa9475fda6ef332f67
soran <soran.taak@gmail.com>
2023-06-07 10:26:07 +0330

    object in javascript

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-function getUserInfo(name, id) {
-	return `Name : ${name} 
-	- ID: ${id}`;
-}
+// let userInfo = {
+// 	userId: 22,
+// 	userName: "Soran",
+// 	role: "Admin",
+// };
 
-console.log(getUserInfo("Sahar", 39));
+// console.log(`User role is ${userInfo.role}`);
+
+let userInfoOne = {
+	userId: 22,
+	username: "Soran",
+	role: "Admin",
+};
+
+let userInfoTwo = {
+	userId: 12,
+	username: "Test",
+	role: "User",
+};
+
+let fetchUserData = function (user) {
+	console.log(`Usernane is : ${user.username}`);
+};
+fetchUserData(userInfoOne);
+fetchUserData(userInfoTwo);
