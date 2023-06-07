7dd8ded2dc7c5439bf9d9a7b41fdad2ec7cf5e84
soran <soran.taak@gmail.com>
2023-06-07 10:17:26 +0330

    template-string-literal

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-let sum = function (num1, num2, num3) {
-	let res = num1 + num2 + num3;
-	return res;
-};
-
-console.log(sum(10, 20, 30));
-
-function getUserInfo(name = "soran", id = 1) {
-	return "Name: " + name + " " + "Id: " + id;
+function getUserInfo(name, id) {
+	return `Name : ${name} 
+	- ID: ${id}`;
 }
 
-console.log(getUserInfo());
 console.log(getUserInfo("Sahar", 39));
