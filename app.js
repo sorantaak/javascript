41e08a004ab5405e15ede0c36904969aa720efb3
soran <soran.taak@gmail.com>
2023-06-07 08:56:54 +0330

    variabels-strings-number

---
+++ app.js
@@ -0,0 +1,14 @@
+// string
+let firstName = "Soran";
+let lastName = "taak";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// number
+let num1 = 12;
+let num2 = 20;
+let sum = num1 + num2;
+console.log(sum);
+
+let res = fullName + num1 + num2;
+console.log(res);
