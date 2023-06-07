e36f57f455d0d11aaeee1f750d103b0706f3dc15
soran <soran.taak@gmail.com>
2023-06-07 09:47:42 +0330

    variable-scope

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-let age = 12;
-if (age > 20) {
-	console.log("Is young");
-}
-if (age < 20) {
-	console.log("Is childe");
-}
+// scope
+// let firstName = "Soran";
+
+// if (true) {
+// 	let lastName = "Taak"; // locale variable
+// 	console.log(lastName);
+// 	// console.log(firstName);
+// 	if (true) {
+// 		let age = 30;
+// 		console.log(age);
+// 	}
+
+// }
+// console.log(firstName);
 
 // new example
-let validEmail = true;
-let validPassword = false;
-if (validEmail && validPassword) {
-	console.log("Logged in");
-} else if (validEmail || validPassword) {
-	console.log("Is Correct");
-}
-
-// new example
-let isUser = true;
-let userRole = "admin";
-
-if (isUser) {
-	console.log("Welcome User!");
-} else if (userRole === "admin") {
-	console.log("Welcome admin");
-} else {
-	console.log("Welcome");
+let firstName = "Test1";
+if (true) {
+	// let firstName = "Test2";
+	if (true) {
+		// let firstName = "Test3";
+		console.log(firstName);
+	}
 }
