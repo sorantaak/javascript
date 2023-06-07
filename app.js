01198d95571b6ebb0ceb05a3b856aab03666a8aa
soran <soran.taak@gmail.com>
2023-06-07 11:11:48 +0330

    object-method

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
-let userAccount = {
-	fullName: "soran taak",
-	inCome: 0,
-	outCome: 0,
+let course = {
+	name: "Advanced Javascript",
+	studentLimit: 30,
+	studentCount: 0,
+	checkAvalibility: function (courseSize) {
+		let leftCount = this.studentLimit - this.studentCount;
+		return courseSize < leftCount
+	},
 };
-
-let addIncome = function (user, amount) {
-	user.inCome += amount;
-};
-let addOutcome = function (user, amount) {
-	user.outCome += amount;
-};
-
-let getBalanceAccoutInfo = function (user) {
-	let res = `${user.fullName} is inCome : ${user.inCome} and outCome is: ${
-		user.outCome
-	}. and balance is:
-	${user.inCome - user.outCome}
-	`;
-	return res;
-};
-
-addIncome(userAccount, 10000);
-addOutcome(userAccount, 4500);
-console.log(getBalanceAccoutInfo(userAccount));
+let status = course.checkAvalibility(30);
+console.log(status);
+// course.checkAvalibility(20);
