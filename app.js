d2c6ff9df3b388ea142ba40b8f8b4996633cf5c6
soran <soran.taak@gmail.com>
2023-06-07 10:51:40 +0330

    challenge-01 calculate inCome adn OutCome for accoutn

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// let userInfo = {
-// 	userId: 22,
-// 	userName: "Soran",
-// 	role: "Admin",
-// };
-
-// console.log(`User role is ${userInfo.role}`);
-
-let userInfoOne = {
-	userId: 22,
-	username: "Soran",
-	role: "Admin",
+let userAccount = {
+	fullName: "soran taak",
+	inCome: 0,
+	outCome: 0,
 };
 
-let userInfoTwo = {
-	userId: 12,
-	username: "Test",
-	role: "User",
+let addIncome = function (user, amount) {
+	user.inCome += amount;
+};
+let addOutcome = function (user, amount) {
+	user.outCome += amount;
 };
 
-let fetchUserData = function (user) {
-	console.log(`Usernane is : ${user.username}`);
+let getBalanceAccoutInfo = function (user) {
+	let res = `${user.fullName} is inCome : ${user.inCome} and outCome is: ${
+		user.outCome
+	}. and balance is:
+	${user.inCome - user.outCome}
+	`;
+	return res;
 };
-fetchUserData(userInfoOne);
-fetchUserData(userInfoTwo);
+
+addIncome(userAccount, 10000);
+addOutcome(userAccount, 4500);
+console.log(getBalanceAccoutInfo(userAccount));
