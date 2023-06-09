c7c5385ad326ce632a8cb5b12cebbff761d0563e
soran <soran.taak@gmail.com>
2023-06-09 18:05:14 +0330

    strings

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-let course = {
-	name: "Advanced Javascript",
-	studentLimit: 30,
-	studentCount: 0,
-	checkAvalibility: function (courseSize) {
-		let leftCount = this.studentLimit - this.studentCount;
-		return courseSize < leftCount
-	},
-};
-let status = course.checkAvalibility(30);
-console.log(status);
-// course.checkAvalibility(20);
+let fullName = "soran_Taak";
+console.log(fullName.length);
+console.log(fullName.toLocaleLowerCase());
+console.log(fullName.toUpperCase());
+
+let passwoerd = "SoranTaak12345678";
+console.log(passwoerd.includes("112345"));
+let email = " soran.taak@gmail.com ";
+console.log(email);
+
+console.log(email.trim());
