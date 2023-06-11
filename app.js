5728e6dfcf1a8616f9ead2e12c3dcd35905bc987
soran <soran.taak@gmail.com>
2023-06-11 11:02:44 +0330

    for loop in javascript

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const cartItems = ["book1", "book2", "book3", "Book4"];
+for (let i = 2; i <= 5; i++) {
+	console.log(i);
+}
 
-// Remove first and third item
-cartItems.shift();
-console.log(cartItems);
-cartItems.splice(1, 1);
-console.log(cartItems);
-// add new item
-cartItems.push("Book5");
-console.log(cartItems);
-// show array product
-cartItems.forEach(function (item, index) {
-	console.log(`${index + 1} - Product Name: ${item}`);
-});
+const cartItems = ["book1", "book2", "book3", "book4"];
+for (let i = 0; i < cartItems.length; i++) {
+	console.log(cartItems[i]);
+}
+
+const cartItems2 = ["ball1", "ball2", "ball3", "ball4"];
+for (let i = 0; i < cartItems2.length; i++) {
+	console.log(`${i + 1} - Prodcut Name : ${cartItems2[i]}`);
+}
