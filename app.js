dde93a33e6fd64ce8444529ae7a3421ba9a44d07
soran <soran.taak@gmail.com>
2023-06-11 11:08:18 +0330

    indexOf method in array

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-for (let i = 2; i <= 5; i++) {
-	console.log(i);
-}
-
 const cartItems = ["book1", "book2", "book3", "book4"];
-for (let i = 0; i < cartItems.length; i++) {
-	console.log(cartItems[i]);
-}
-
-const cartItems2 = ["ball1", "ball2", "ball3", "ball4"];
-for (let i = 0; i < cartItems2.length; i++) {
-	console.log(`${i + 1} - Prodcut Name : ${cartItems2[i]}`);
-}
+console.log(cartItems.indexOf("book3", 0));
+console.log(cartItems.indexOf("test", 0));
