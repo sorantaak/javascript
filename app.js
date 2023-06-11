63b4b06145689b680c4ffc380fa3ceece2b9beb4
soran <soran.taak@gmail.com>
2023-06-11 11:25:13 +0330

    findIndex method in arry

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-const cartItems = ["book1", "book2", "book3", "book4"];
-console.log(cartItems.indexOf("book3", 0));
-console.log(cartItems.indexOf("book3"));
-console.log(cartItems.indexOf("test", 0));
+const cartItems = [
+	{ title: "Book1", price: 29 },
+	{ title: "Book2", price: 59 },
+	{ title: "Book3", price: 79 },
+];
+
+// const indexValue = cartItem.findIndex(function (item, index) {
+// 	return item.title === "Book 2";
+// });
+// console.log(indexValue);
+const findProducts = function (cart, productTitle) {
+	const indexValue = cart.findIndex(function (item, index) {
+		return item.title.toLowerCase() === productTitle.toLowerCase();
+	});
+
+	return indexValue > 0
+		? cart[indexValue]
+		: `could not find product :${productTitle}`;
+};
+const result = findProducts(cartItems, "aaaa");
+console.log(result);
