1cea191ca0ffb151539fb9b6861553bdf817f7a7
soran <soran.taak@gmail.com>
2023-06-11 10:35:35 +0330

    array methods

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const tasks = ["Task1", "Task2", "Task3", 12, true];
-console.log(tasks.length);
-console.log(tasks[2]); // access index 2 values
+const cartItems = ["book1", "book2", "book3", "Book4"];
+cartItems.push("Book5"); // Append new element end of array
+console.log(cartItems);
+cartItems.pop(); // remove the last element fron an array and return it.
+console.log(cartItems);
+console.log("new Lenght of Array is : " + cartItems.unshift("Book0")); // Insert new elements at the start of array  and return the new length of Array
+console.log(cartItems);
 
-//
-const cartItems = ["book1", "book2", "book3"];
-let result = `You have ${cartItems.length} products in your cart`;
-console.log(result);
-console.log(`First item is ${cartItems[0]}`);
-console.log(`First item is ${cartItems[cartItems.length - 1]}`);
+console.log(
+	"remove first element by shift array method and it is: " + cartItems.shift()
+); // remove first element of an array and returns it
+
+console.log(cartItems);
+
+// splcie method
+cartItems.splice(0, 2);
+console.log("after splice start 0 and countdelete is 2 : ", cartItems);
+
+cartItems.splice(0, 2, "new Item");
+console.log("after splice start 0 and countdelete is 2 : ", cartItems);
