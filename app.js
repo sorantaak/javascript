2a615b85c20fbf83c3e195818a855dc721988be5
soran <soran.taak@gmail.com>
2023-06-11 12:29:03 +0330

    challenge for find and remove element in array by findIndex and splice mehtods

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ const cartItems = [
 	{ title: "Book1", price: 29 },
 	{ title: "Book2", price: 59 },
 	{ title: "Book3", price: 79 },
+	{ title: "Book4", price: 79 },
+	{ title: "Book5", price: 79 },
 ];
 
-// const indexValue = cartItem.findIndex(function (item, index) {
-// 	return item.title === "Book 2";
-// });
-// console.log(indexValue);
-
-const findProducts = function (cart, productTitle) {
-	const product = cart.find(function (item, index) {
-		return item.title.toLowerCase() === productTitle.toLowerCase(); // if condition is true return current item
+const deleteProduct = function (products, titleProduct) {
+	const valIndex = products.findIndex(function (item) {
+		return item.title.toLowerCase() === titleProduct.toLowerCase();
 	});
-	// console.log(product);
-	return product !== undefined
-		? product
-		: `could not find product :${productTitle}`;
+	console.log(valIndex);
+	if (valIndex > -1) {
+		products.splice(valIndex, 1);
+		return products;
+	} else {
+		return `can not find this product :${titleProduct}`;
+	}
 };
-const result = findProducts(cartItems, "book3");
-console.log(result);
+
+console.log(deleteProduct(cartItems, "book4"));
