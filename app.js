c6abef43cce6d32fc36e9237419849f6c55944ef
soran <soran.taak@gmail.com>
2023-06-11 09:43:40 +0330

    learn Array concept in javascript

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-// const isUSer = true;
-// isUSer = false;
-// console.log(isUSer);
-const userInfo = {
-	id: 2,
-};
+const tasks = ["Task1", "Task2", "Task3", 12, true];
+console.log(tasks.length);
+console.log(tasks[2]); // access index 2 values
 
-// userInfo = {
-// 	id:21
-// }
-userInfo.id = 40;
-console.log(userInfo);
+//
+const cartItems = ["book1", "book2", "book3"];
+let result = `You have ${cartItems.length} products in your cart`;
+console.log(result);
+console.log(`First item is ${cartItems[0]}`);
+console.log(`First item is ${cartItems[cartItems.length - 1]}`);
