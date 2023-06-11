32d473809430ad05ee78d3db023a11ab553bf9f5
soran <soran.taak@gmail.com>
2023-06-11 11:09:48 +0330

    add one line for test indexOf method

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const cartItems = ["book1", "book2", "book3", "book4"];
 console.log(cartItems.indexOf("book3", 0));
+console.log(cartItems.indexOf("book3"));
 console.log(cartItems.indexOf("test", 0));
