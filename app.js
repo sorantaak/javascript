fe8c0ed2be2094d87c1f08c43cea97133fcf2071
soran <soran.taak@gmail.com>
2023-06-12 11:14:16 +0330

    sort method in array

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// const numbers = [12, 22, 9, 3, 2];
-// const filterNumbers = numbers.filter(function (item, index) {
-// 	return item >= 10;
-// });
-// console.log(filterNumbers);
 const productItem = [
 	{ title: "Book1", price: 29, exist: true },
 	{ title: "Book2", price: 59, exist: false },
@@ -10,10 +5,16 @@ const productItem = [
 	{ title: "Book4", price: 41, exist: false },
 	{ title: "Book5", price: 98, exist: true },
 ];
-const productNotExist = function (products) {
-	return products.filter((item) => {
-		return !item.exist;
+const sortProducts = function (products) {
+	products.sort(function (firstEl, secEl) {
+		if (firstEl.exist === true && secEl.exist === false) {
+			return -1;
+		} else if (secEl.exist === true && firstEl.exist === false) {
+			return 1;
+		} else {
+			return 0;
+		}
 	});
 };
-const result = productNotExist(productItem);
-console.log(result);
+sortProducts(productItem);
+console.log(productItem);
