e75eb5f9fde4d47433c7494eebf123198519d8bf
soran <soran.taak@gmail.com>
2023-06-12 11:04:52 +0330

    filter methods in array

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-const cartItems = [
-	{ title: "Book1", price: 29 },
-	{ title: "Book2", price: 59 },
-	{ title: "Book3", price: 79 },
-	{ title: "Book4", price: 79 },
-	{ title: "Book5", price: 79 },
+// const numbers = [12, 22, 9, 3, 2];
+// const filterNumbers = numbers.filter(function (item, index) {
+// 	return item >= 10;
+// });
+// console.log(filterNumbers);
+const productItem = [
+	{ title: "Book1", price: 29, exist: true },
+	{ title: "Book2", price: 59, exist: false },
+	{ title: "Book3", price: 32, exist: true },
+	{ title: "Book4", price: 41, exist: false },
+	{ title: "Book5", price: 98, exist: true },
 ];
-
-const deleteProduct = function (products, titleProduct) {
-	const valIndex = products.findIndex(function (item) {
-		return item.title.toLowerCase() === titleProduct.toLowerCase();
+const productNotExist = function (products) {
+	return products.filter((item) => {
+		return !item.exist;
 	});
-	console.log(valIndex);
-	if (valIndex > -1) {
-		products.splice(valIndex, 1);
-		return products;
-	} else {
-		return `can not find this product :${titleProduct}`;
-	}
 };
-
-console.log(deleteProduct(cartItems, "book4"));
+const result = productNotExist(productItem);
+console.log(result);
