183a7a04e109ca561fddc9d6a605dd7b464455b5
soran <soran.taak@gmail.com>
2023-06-18 20:23:09 +0330

    dom-review

--- web-app/app.js
+++ web-app/app.js
@@ -1,1 +1,13 @@
-console.log("This is form app.js file");
+// console.log(document);
+// const paragraph = document.querySelector("p");
+// console.log(paragraph);
+// paragraph.remove();
+
+const ps = document.querySelectorAll("p");
+console.log(ps);
+ps.forEach((item) => {
+	// console.log(item.textContent);
+	// item.remove();
+	item.textContent = "Update P "; // change content in all p tags
+	console.log(item.textContent);
+});
