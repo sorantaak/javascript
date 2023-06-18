0495176ffbc84139e536bc8bf28c9692d8bbeb6d
soran <soran.taak@gmail.com>
2023-06-18 21:07:01 +0330

    challenge for check how many books is exist in store and show all title books

--- web-app/app.js
+++ web-app/app.js
@@ -1,5 +1,18 @@
-const newParagraph = document.createElement("p");
-newParagraph.textContent = "This is new Book";
+const productItem = [
+	{ title: "Book1", exist: true },
+	{ title: "Book2", exist: false },
+	{ title: "Book3", exist: true },
+	{ title: "Book4", exist: false },
+];
+const productExists = productItem.filter((item) => {
+	return item.exist;
+});
+const existProduct = document.createElement("h2");
+existProduct.textContent = `Number of avilable products : ${productExists.length}`;
+document.querySelector("body").appendChild(existProduct);
 
-const body = document.querySelector("body");
-body.appendChild(newParagraph);
+productItem.forEach((item) => {
+	const p = document.createElement("p");
+	p.textContent = item.title;
+	document.querySelector("body").appendChild(p);
+});
