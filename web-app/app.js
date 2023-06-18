b1fe6c562a2a4f2a00cc12560230ea090d87c45b
soran <soran.taak@gmail.com>
2023-06-18 20:34:03 +0330

    challenge to remove paragraph

--- web-app/app.js
+++ web-app/app.js
@@ -1,13 +1,4 @@
-// console.log(document);
-// const paragraph = document.querySelector("p");
-// console.log(paragraph);
-// paragraph.remove();
-
-const ps = document.querySelectorAll("p");
-console.log(ps);
-ps.forEach((item) => {
-	// console.log(item.textContent);
-	// item.remove();
-	item.textContent = "Update P "; // change content in all p tags
-	console.log(item.textContent);
+const paragraphs = document.querySelectorAll("p");
+paragraphs.forEach((item) => {
+	item.textContent.toLowerCase().includes("js") && item.remove();
 });
