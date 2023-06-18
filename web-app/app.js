5da9e60d8213b68d8d7f128cc065dc5665af1fa2
soran <soran.taak@gmail.com>
2023-06-18 21:49:56 +0330

    button event

--- web-app/app.js
+++ web-app/app.js
@@ -1,18 +1,5 @@
-const productItem = [
-	{ title: "Book1", exist: true },
-	{ title: "Book2", exist: false },
-	{ title: "Book3", exist: true },
-	{ title: "Book4", exist: false },
-];
-const productExists = productItem.filter((item) => {
-	return item.exist;
-});
-const existProduct = document.createElement("h2");
-existProduct.textContent = `Number of avilable products : ${productExists.length}`;
-document.querySelector("body").appendChild(existProduct);
+const btn = document.querySelector("button");
 
-productItem.forEach((item) => {
-	const p = document.createElement("p");
-	p.textContent = item.title;
-	document.querySelector("body").appendChild(p);
+btn.addEventListener("click", (e) => {
+	e.target.textContent = "Clicked";
 });
