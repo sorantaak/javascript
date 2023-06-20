07125f8b392c523ab67d9f9c12d51cc28a0350ee
soran <soran.taak@gmail.com>
2023-06-20 11:33:32 +0330

    checkbox in javascript

--- web-app/app.js
+++ web-app/app.js
@@ -1,33 +1,3 @@
-const products = [];
-const filters = {
-	searchItem: "",
-};
-
-const renderProducts = function (products, filters) {
-	const filterProdcuts = products.filter(function (item) {
-		return item.title.toLowerCase().includes(filters.searchItem.toLowerCase());
-	});
-	document.querySelector("#products").innerHTML = "";
-	filterProdcuts.forEach(function (item) {
-		const productEl = document.createElement("p");
-		productEl.textContent = item.title;
-		document.querySelector("#products").appendChild(productEl);
-	});
-};
-renderProducts(products, filters);
-
-document
-	.querySelector("#search-products")
-	.addEventListener("input", function (e) {
-		filters.searchItem = e.target.value;
-		renderProducts(products, filters);
-	});
-
-document
-	.querySelector("#add-product-form")
-	.addEventListener("submit", function (e) {
-		e.preventDefault();
-		products.push({ title: e.target.elements.productTitle.value, exist: true });
-		renderProducts(products, filters);
-		e.target.elements.productTitle.value = "";
-	});
+document.querySelector("#checkbox").addEventListener("change", function (e) {
+	console.log(e.target.checked);
+});
