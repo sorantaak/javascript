c9528d6f76bdb55e444c1ee7a6d3662b9a874bbc
soran <soran.taak@gmail.com>
2023-06-20 10:34:18 +0330

    form in javascript

--- web-app/app.js
+++ web-app/app.js
@@ -1,31 +1,7 @@
-const products = [
-	{ title: "Node.js Design Pattern" },
-	{ title: "You Dont Know JS: this & Object Prototypes" },
-	{ title: "Functional React" },
-	{ title: "You Dont Know JS: Async & Performance" },
-];
-const filters = {
-	searchItem: "",
-};
-
-const rennderProducts = function (products, filters) {
-	const filterProducts = products.filter(function (item) {
-		return item.title.toLowerCase().includes(filters.searchItem.toLowerCase());
-	});
-	document.querySelector("#products").innerHTML = "";
-	filterProducts.forEach(function (item) {
-		const p = document.createElement("p");
-		p.textContent = item.title;
-
-		document.querySelector("#products").appendChild(p);
-	});
-};
-
-rennderProducts(products, filters);
-
 document
-	.querySelector("#search-products")
-	.addEventListener("input", function (e) {
-		filters.searchItem = e.target.value;
-		rennderProducts(products, filters);
+	.querySelector("#add-product-form")
+	.addEventListener("submit", function (e) {
+		e.preventDefault();
+		console.log(e.target.elements.productTitle.value);
+		e.target.elements.productTitle.value = "";
 	});
