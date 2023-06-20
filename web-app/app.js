ae6a581bbe1d8d6e1c633f7e5e11248a4d5ea45a
soran <soran.taak@gmail.com>
2023-06-20 10:02:32 +0330

    search in elements

--- web-app/app.js
+++ web-app/app.js
@@ -1,6 +1,25 @@
-document.querySelector("#search-products").addEventListener("input", (e) => {
-	console.log("input : " + e.target.value);
-});
-document.querySelector("#search-products").addEventListener("change", (e) => {
-	console.log("change : " + e.target.value);
-});
+const products = [
+	{ title: "Node.js Design Pattern" },
+	{ title: "You Dont Know JS: this & Object Prototypes" },
+	{ title: "Functional React" },
+	{ title: "You Dont Know JS: Async & Performance" },
+];
+const filters = {
+	searchItem: "",
+};
+
+const rennderProducts = function (products, filters) {
+	const filterProducts = products.filter(function (item) {
+		return item.title.toLowerCase().includes(filters.searchItem.toLowerCase());
+	});
+	console.log(filterProducts);
+};
+
+rennderProducts(products, filters);
+
+document
+	.querySelector("#search-products")
+	.addEventListener("input", function (e) {
+		filters.searchItem = e.target.value;
+		rennderProducts(products, filters);
+	});
