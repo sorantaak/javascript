1c8a14b0640399f87fefd26ca48f3b67d8c03d70
soran <soran.taak@gmail.com>
2023-06-21 10:32:07 +0330

    create elements for item

--- web-app/functions.js
+++ web-app/functions.js
@@ -29,7 +29,19 @@ const renderProducts = function (products, filters) {
 };
 
 const createProductDOM = function (product) {
-	const productEl = document.createElement("p");
-	productEl.textContent = product.title;
+	const productEl = document.createElement("div");
+	const checkBox = document.createElement("input");
+	const productItem = document.createElement("span");
+	const removeButton = document.createElement("button");
+
+	checkBox.setAttribute("type", "checkbox");
+	productEl.appendChild(checkBox);
+
+	productItem.textContent = product.title;
+	productEl.appendChild(productItem);
+
+	removeButton.textContent = "Remove";
+	productEl.appendChild(removeButton);
+
 	return productEl;
 };
