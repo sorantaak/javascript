187bee5309c7ab37d27b712eb3bd59fc3ed72e97
soran <soran.taak@gmail.com>
2023-07-04 09:49:20 +0330

    challeng create edit product page

--- web-app/edit-product.js
+++ web-app/edit-product.js
@@ -1,5 +1,6 @@
 const titleElement = document.querySelector("#product-title");
 const priceeElement = document.querySelector("#product-price");
+const removeBtn = document.createElement("button");
 // console.log(location.hash.substring(1));
 const productId = location.hash.substring(1);
 const products = getSaveProducts();
@@ -14,4 +15,34 @@ if (product === undefined) {
 }
 
 titleElement.value = product.title;
-titlePrice.value = product.price;
+priceeElement.value = product.price;
+
+titleElement.addEventListener("input", function (e) {
+	console.log(e.target.value);
+	products.map(function (item) {
+		if (item.id === productId) {
+			item.title = e.target.value;
+		}
+		console.log(item);
+	});
+	saveProducts(products);
+});
+priceeElement.addEventListener("input", function (e) {
+	console.log(e.target.value);
+	products.map(function (item) {
+		if (item.id === productId) {
+			item.price = e.target.value;
+		}
+		console.log(item);
+	});
+	saveProducts(products);
+});
+removeBtn.addEventListener("click", function () {
+	const newProdcuts = products.filter(function (item) {
+		return item.id !== productId;
+	});
+	saveProducts(newProdcuts);
+	location.assign("/index.html");
+});
+removeBtn.textContent = `remove ${product.title}`;
+document.body.appendChild(removeBtn);
