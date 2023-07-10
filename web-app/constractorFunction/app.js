78a67c60fb0703addc2efef3b9ee459ed2bd7c37
soran <soran.taak@gmail.com>
2023-07-10 08:16:08 +0330

    add method by prototype to constractor function

--- web-app/constractorFunction/app.js
+++ web-app/constractorFunction/app.js
@@ -6,10 +6,15 @@
 // 	},
 // };
 
-const User = function (email) { // there is User is constractor. 
+const User = function (email, id) {
+	// there is User is constractor.
 	this.email = email;
+	this.id - id;
 };
-const username = new User("test@test.com"); // when use new perfrix function infact we created object from function. username is object now => {} . user is constractor
-console.log(username);
-const username2 = new User("test2@test.com");
-console.log(username2);
+User.prototype.userInfo = function () {
+	return `ID: ${this.id} - Email: ${this.email}`;
+};
+const username = new User("test@test.com", 2); // when use new perfrix function infact we created object from function. username is object now => {} . user is constractor
+console.log(username.userInfo());
+const username2 = new User("test2@test2.com", 6);
+console.log(username2.userInfo());
