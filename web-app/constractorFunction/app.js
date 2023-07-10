9f49473a14ce9a99ce5e7ed45038c3e798a616c4
soran <soran.taak@gmail.com>
2023-07-10 10:46:03 +0330

    promise in javascript

--- web-app/constractorFunction/app.js
+++ web-app/constractorFunction/app.js
@@ -12,15 +12,29 @@ getProducts = () => {
 		console.log(fetchProduct);
 	}, 2000);
 };
-crateProduct = (callback) => {
-	setTimeout(() => {
-		products.push({
-			title: "New Book",
-			price: 99,
-		});
-		callback();
-	}, 3000);
+
+crateProduct = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			products.push({
+				title: "New Book",
+				price: 99,
+			});
+			const isCheck = true;
+			if (!isCheck) {
+				resolve();
+			} else {
+				reject("Error");
+			}
+		}, 3000);
+	});
 };
-
-crateProduct(getProducts);
-
+console.log(crateProduct);
+// crateProduct()
+// 	.then(getProducts)
+// 	.catche((er) => {
+// 		console.log(er);
+// 	});
+crateProduct()
+	.then(getProducts)
+	.catch((er) => console.log(er));
