a4ca1cad5ee24e7a32b69a7ac1c0c9e64ea17759
soran <soran.taak@gmail.com>
2023-07-10 08:29:38 +0330

    subclasses in javascript

--- web-app/constractorFunction/app.js
+++ web-app/constractorFunction/app.js
@@ -1,15 +1,3 @@
-// const User = function (id, email) {
-// 	// there is User is constractor.
-// 	this.email = email;
-// 	this.id - id;
-// };
-// User.prototype.userInfo = function () {
-// 	return `ID: ${this.id} - Email: ${this.email}`;
-// };
-// const username = new User(2, "test@test.com");
-// console.log(username.userInfo());
-// const username2 = new User(6, "test2@test2.com");
-// console.log(username2.userInfo());
 class User {
 	constructor(id, email) {
 		this.id = id;
@@ -19,5 +7,12 @@ class User {
 		return `ID: ${this.id} - Email: ${this.email}`;
 	}
 }
-const user1 = new User(2, "test@test.com");
-console.log(user1.userInfo());
+
+class Job extends User {
+	constructor(id, email, jobTitle) {
+		super(id, email);
+		this.jobTitle = jobTitle;
+	}
+}
+const user1 = new Job(2, "test@test.com", "developer");
+console.log(user1);
