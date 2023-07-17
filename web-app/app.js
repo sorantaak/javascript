5681062d152cb2b17085f0e478b9c805228c752e
soran <soran.taak@gmail.com>
2023-07-17 10:04:39 +0330

    xmlHttpRequest post

--- web-app/app.js
+++ web-app/app.js
@@ -1,18 +1,22 @@
 const getBtn = document.querySelector("#get-btn");
 const postBtn = document.querySelector("#post-btn");
 
-const sendHttpRequest = (method, url) => {
+const sendHttpRequest = (method, url, data) => {
 	return new Promise((resolve, reject) => {
 		const xhr = new XMLHttpRequest();
 		xhr.open(method, url);
 		xhr.responseType = "json";
+		if (data) {
+			xhr.setRequestHeader("Content-Type", "application/json");
+		}
 		xhr.onload = () => {
 			resolve(xhr.response);
 		};
 		xhr.onerror = () => {
 			reject("Error");
 		};
-		xhr.send();
+
+		data ? xhr.send(JSON.stringify(data)) : xhr.send();
 	});
 };
 
@@ -22,7 +26,16 @@ const getData = () => {
 		.catch((er) => console.log(er));
 };
 
-const postData = () => {};
+const postData = () => {
+	sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", {
+		userId: 2,
+		id: 2,
+		title: "Post Title",
+		body: "Post Body",
+	})
+		.then((res) => console.log(res))
+		.catch((er) => console.log(er));
+};
 
 getBtn.addEventListener("click", getData);
 postBtn.addEventListener("click", postData);
