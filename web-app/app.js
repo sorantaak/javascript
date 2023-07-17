f50922b72cd49be3a1ecabd19646e169b8e2cf0f
soran <soran.taak@gmail.com>
2023-07-17 08:50:59 +0330

    xmlHttpRequest

--- web-app/app.js
+++ web-app/app.js
@@ -1,50 +1,17 @@
-let products = getSaveProducts();
+const getBtn = document.querySelector("#get-btn");
+const postBtn = document.querySelector("#post-btn");
 
-const filters = {
-	searchItem: "",
-	availableProducts: false,
-	sortBy: "byEdited",
+const getData = () => {
+	const xhr = new XMLHttpRequest();
+	xhr.open("GET", "https://jsonplaceholder.typicode.com/posts/1");
+	xhr.responseType = "json";
+	xhr.onload = () => {
+		console.log(xhr.response);
+	};
+	xhr.send();
 };
-renderProducts(products, filters);
-document.querySelector("#search-products").addEventListener("input", (e) => {
-	filters.searchItem = e.target.value;
-	renderProducts(products, filters);
-});
-document.querySelector("#add-product-form").addEventListener("submit", (e) => {
-	e.preventDefault();
-	const id = uuidv4();
-	const timestamp = moment().valueOf(); // create timestamp as number by valueOf method
-	products.push({
-		id: id,
-		title: e.target.elements.productTitle.value,
-		price: "",
-		exist: true,
-		created: timestamp,
-		updated: timestamp,
-	});
-	saveProducts(products);
-	renderProducts(products, filters);
-	e.target.elements.productTitle.value = "";
-});
 
-document
-	.querySelector("#available-products")
-	.addEventListener("change", (e) => {
-		console.log(e.target.checked);
-		filters.availableProducts = e.target.checked;
-		renderProducts(products, filters);
-	});
+const postData = () => {};
 
-window.addEventListener("storage", (e) => {
-	if (e.key === "products") {
-		console.log(JSON.parse(e.newValue));
-		products = JSON.parse(e.newValue);
-		renderProducts(products, filters);
-	}
-});
-
-document.querySelector("#sort").addEventListener("change", (e) => {
-	// console.log(e.target.value);
-	filters.sortBy = e.target.value;
-	renderProducts(products, filters);
-});
+getBtn.addEventListener("click", getData);
+postBtn.addEventListener("click", postData);
