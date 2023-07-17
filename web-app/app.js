f0f51fb80ab754ead9872ab9394c8e4c8a3506a1
soran <soran.taak@gmail.com>
2023-07-17 10:20:57 +0330

    getch api get data

--- web-app/app.js
+++ web-app/app.js
@@ -2,21 +2,8 @@ const getBtn = document.querySelector("#get-btn");
 const postBtn = document.querySelector("#post-btn");
 
 const sendHttpRequest = (method, url, data) => {
-	return new Promise((resolve, reject) => {
-		const xhr = new XMLHttpRequest();
-		xhr.open(method, url);
-		xhr.responseType = "json";
-		if (data) {
-			xhr.setRequestHeader("Content-Type", "application/json");
-		}
-		xhr.onload = () => {
-			resolve(xhr.response);
-		};
-		xhr.onerror = () => {
-			reject("Error");
-		};
-
-		data ? xhr.send(JSON.stringify(data)) : xhr.send();
+	return fetch(url).then((res) => {
+		return res.json();
 	});
 };
 
@@ -26,16 +13,7 @@ const getData = () => {
 		.catch((er) => console.log(er));
 };
 
-const postData = () => {
-	sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", {
-		userId: 2,
-		id: 2,
-		title: "Post Title",
-		body: "Post Body",
-	})
-		.then((res) => console.log(res))
-		.catch((er) => console.log(er));
-};
+const postData = () => {};
 
 getBtn.addEventListener("click", getData);
 postBtn.addEventListener("click", postData);
