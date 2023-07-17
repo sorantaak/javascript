c94836f5a5ff83c6daaf744f0a43e73ab4193a95
soran <soran.taak@gmail.com>
2023-07-17 09:54:05 +0330

    add promise in xmlHttpRequest

--- web-app/app.js
+++ web-app/app.js
@@ -1,14 +1,25 @@
 const getBtn = document.querySelector("#get-btn");
 const postBtn = document.querySelector("#post-btn");
 
+const sendHttpRequest = (method, url) => {
+	return new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
+		xhr.open(method, url);
+		xhr.responseType = "json";
+		xhr.onload = () => {
+			resolve(xhr.response);
+		};
+		xhr.onerror = () => {
+			reject("Error");
+		};
+		xhr.send();
+	});
+};
+
 const getData = () => {
-	const xhr = new XMLHttpRequest();
-	xhr.open("GET", "https://jsonplaceholder.typicode.com/posts/1");
-	xhr.responseType = "json";
-	xhr.onload = () => {
-		console.log(xhr.response);
-	};
-	xhr.send();
+	sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/posts/1")
+		.then((res) => console.log(res))
+		.catch((er) => console.log(er));
 };
 
 const postData = () => {};
