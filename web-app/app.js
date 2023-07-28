4990921c2f2c29922b442a514c89c8da9120d2c0
soran <soran.taak@gmail.com>
2023-07-28 08:18:14 +0330

    axios get and post data

--- web-app/app.js
+++ web-app/app.js
@@ -1,35 +1,21 @@
 const getBtn = document.querySelector("#get-btn");
 const postBtn = document.querySelector("#post-btn");
 
-const sendHttpRequest = (method, url, data) => {
-	console.log(data);
-	return fetch(url, {
-		method: method,
-		body: JSON.stringify(data),
-		headers: data
-			? {
-					"Content-Type": "application/json",
-					// 'Content-Type': 'application/x-www-form-urlencoded',
-			  }
-			: {},
-	}).then((res) => {
-		return res.json();
-	});
-};
-
 const getData = () => {
-	sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/posts/1")
-		.then((res) => console.log(res))
-		.catch((er) => console.log(er));
+	axios
+		.get("https://jsonplaceholder.typicode.com/posts/1")
+		.then((res) => console.log(res));
 };
 
 const postData = () => {
-	sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", {
-		userId: 3,
-		id: 107,
-		title: "Post title",
-		body: "Post body",
-	}).then((res) => console.log(res));
+	axios
+		.post("https://jsonplaceholder.typicode.com/posts", {
+			userId: 3,
+			id: 107,
+			title: "Post title",
+			body: "Post body",
+		})
+		.then((res) => console.log(res));
 };
 
 getBtn.addEventListener("click", getData);
